5610f49e097511d72e2acdb687ad732c8a6f0d87
ronald <ronaldodongo76@gmail.com>
2024-10-27 10:15:06 +0300

    server port binfing update

--- backend/server.js
+++ backend/server.js
@@ -5,34 +5,32 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
 const bookRoutes = require('./routes/books');
-const analyticsRoutes = require('./routes/analytics'); // Import analytics routes
+const analyticsRoutes = require('./routes/analytics');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
-app.use('/api/analytics', analyticsRoutes); // Use analytics routes
+app.use('/api/analytics', analyticsRoutes);
 
-// Use strictQuery to avoid deprecation warnings
 mongoose.set('strictQuery', true);
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => {
-    app.listen(PORT, () => {
+    console.log(`Connected to MongoDB`);
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
     console.error('MongoDB connection error:', error);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   });
 
