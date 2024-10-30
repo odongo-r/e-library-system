d8fa4d0f84d9757e74eab969f61dcb60a93047ed
ronald <ronaldodongo76@gmail.com>
2024-10-30 19:52:50 +0300

    book add  update

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,10 +1,13 @@
+// models/Book.js
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  description: String,
-  publishedYear: Number,
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  publicationDate: { type: Date, required: true },
+  description: { type: String },
+  publishedYear: { type: Number }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
