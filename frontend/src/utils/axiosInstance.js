ce1f40a89c1489db3a3118e79adac6d7d6dd3ddc
ronald <ronaldodongo76@gmail.com>
2024-11-14 20:46:47 +0300

    update axiosInstance

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,16 @@
+/* frontend/src/utils/axiosInstance.js */
 import axios from 'axios';
+
+const token = localStorage.getItem('token'); // Get the token from localStorage
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
 
 export default axiosInstance;
+
+
+
